d61a3d562c7474d4d17b1fc0f3ba8083dbae57e8
kevinraafael <kevinfrancelino@gmail.com>
2023-04-04 20:28:21 -0400

    feat: created two instances of dio - one to get  movie information and another to get movie image

--- lib/app/modules/home/home_binding.dart
+++ lib/app/modules/home/home_binding.dart
@@ -2,8 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 import '../../core/config/env_config.dart';
-import '../../core/config/http.dart';
-import '../../core/interface/http_interface.dart';
+import '../../core/config/http_interceptor.dart';
 import '../../repositories/movies/movies_repository.dart';
 import 'home_controller.dart';
 
@@ -13,12 +12,29 @@ class HomeBinding extends Bindings {
     Get.lazyPut(() => HomeController());
 
     // Get.put((i) => Http(dio: i.get()));
-    Get.put<Dio>(Dio(BaseOptions(baseUrl: EnvConfig.baseUrl))); //
+    Get.put<Dio>(
+        Dio(
+          BaseOptions(baseUrl: EnvConfig.baseUrl),
+        ),
+        tag: 'dioMovie'); //
 
-    Get.put<HttpInterface>(Http(
-      dio: Get.find<Dio>(),
-    ));
+    Get.put<Dio>(
+        Dio(
+          BaseOptions(baseUrl: EnvConfig.baseUrlImage),
+        ),
+        tag: 'dioImage'); //
+
+    Get.put<HttpInterceptor>(
+      HttpInterceptor(
+        dio: Get.find<Dio>(tag: 'dioMovie'),
+      ),
+    );
+
     Get.put<MoviesRepository>(
-        MoviesRepository(http: Get.find<HttpInterface>()));
+      MoviesRepository(
+        dioMovie: Get.find<Dio>(tag: 'dioMovie'),
+        dioImage: Get.find<Dio>(tag: 'dioImage'),
+      ),
+    );
   }
 }
