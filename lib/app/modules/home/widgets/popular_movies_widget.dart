c2ab0d239bfe160ebccc7550232101513de7bc00
kevinraafael <kevinfrancelino@gmail.com>
2023-06-04 14:55:37 -0400

    feature:added cache network image package

--- lib/app/modules/home/widgets/popular_movies_widget.dart
+++ lib/app/modules/home/widgets/popular_movies_widget.dart
@@ -1,10 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
 import '../../../models/movie_model.dart';
 import 'counter_movie_widget.dart';
 
 class PopularMoviesWidget extends StatelessWidget {
   final List<MovieModel>? popularMoviesModel;
+
   const PopularMoviesWidget({
     super.key,
     required this.popularMoviesModel,
@@ -14,7 +15,8 @@ class PopularMoviesWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.25,
-      child: ListView.builder(
+      child: ListView.separated(
+        separatorBuilder: (context, index) => const SizedBox(width: 20),
         scrollDirection: Axis.horizontal,
         shrinkWrap: true,
         itemCount: popularMoviesModel!.length,
@@ -28,12 +30,20 @@ class PopularMoviesWidget extends StatelessWidget {
                 SizedBox(
                   height: MediaQuery.of(context).size.height * 0.20,
                   child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.network(
-                      'https://image.tmdb.org/t/p/original/${popularMovies.posterPath}',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+                      borderRadius: BorderRadius.circular(16),
+                      child: //isImageLoaded[index]?
+                          Container(
+                        color: Colors.grey[50],
+                        child: CachedNetworkImage(
+                          memCacheWidth: 294,
+                          memCacheHeight: 441,
+                          imageUrl:
+                              'https://image.tmdb.org/t/p/original/${popularMovies.posterPath}',
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                      // : const CircularProgressIndicator(),
+                      ),
                 ),
                 Positioned(
                   bottom: 0,
--- lib/main_app.dart
+++ lib/main_app.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    debugInvertOversizedImages = true;
     return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
