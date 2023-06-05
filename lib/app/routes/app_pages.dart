42fb5c42c989244cc9f86a82d598236ac9f3909f
kevinraafael <kevinfrancelino@gmail.com>
2023-06-05 16:37:09 -0400

    refactor:fixed imports

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,8 +1,8 @@
 import 'package:get/get_navigation/get_navigation.dart';
-import '../modules/movie_details/movie_details_page.dart';
 
 import '../app_module.dart';
 import '../modules/home/home_page.dart';
+import '../modules/movie_details/movie_details_page.dart';
 import 'app_routes.dart';
 
 class AppPages {
